84fd2cbc0f17b7401cbb159b01c1a9ac5ab20333
summerzhouty <summer_zhouty@163.com>
2017-01-24 14:32:41 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,31 +2,33 @@
 ## If we want to calculate an inverse matrix which has been already calculated before, instead of re-calculate it again, 
 ## we can just store it in a cache first, and directly recall it from the cache without time-consuming re-calculation.
 
-## Write a short comment describing this function
+## makeCacheMatrix function builds 4 basic functions and returns the functions within a list to the parent environment
 
-makeCacheMatrix <- function(x = matrix()) {
-    IM <- NULL
-    set <- function(y){
+makeCacheMatrix <- function(x = matrix()) { ## assign default value to argument x, which is an empty matrix
+    IM <- NULL ## initialize IM as an object, which need to be used later
+    set <- function(y){   ## set the value of the matrix and clear IM in memory
         x <<- y
         IM <<- NULL
     }
-    get <- function() x
-    setIM <- function(y) IM <<- y
-    getIM <- function() IM
-    list(set=set, get=get, setIM=setIM, getIM=getIM)
+    get <- function() x  ## get the value of the matrix
+    setIM <- function(y) IM <<- y  ## set the value of inverse matrix
+    getIM <- function() IM ## get the value of inverse matrix
+    list(set=set, get=get, setIM=setIM, getIM=getIM) ## return to a list which contains four functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function first checks whether the value of inverse matrix has already been stored in cache.
+## If yes, this function would return the value stored in cache;
+## If no, this function would calculate it, and store it in cache so that we can directly use it next time.
 
 cacheSolve <- function(x, ...) {
-    IM <- x$getIM()
-    if(!is.null(IM)){
+    IM <- x$getIM()  ## get value stored in getIM()
+    if(!is.null(IM)){  ## check whether the value is NULL or not
         message("getting cached data")
-        return(IM)
+        return(IM)  ## if exist, just break the cacheSolve function and return the value stored in cache
     }
-    data <- x$get()
-    IM <- solve(data)
-    x$setIM(IM)
-    IM
+    data <- x$get() ## get data by calling function get()
+    IM <- solve(data)  ## assign the value of inverse matrix to IM
+    x$setIM(IM) ## set IM to the cache
+    IM ## return the value of IM
 }
